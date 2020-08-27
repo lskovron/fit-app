d06cf4789864ccf9cae8fa42b3d474b6f38039cb
Leo Skovron <leo.skovron@emboldhealth.com>
2020-08-27 16:07:54 -0500

    include build

---
+++ build/precache-manifest.35cd73c83a04388554b95e17ac304246.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0940f2e235e4aa23d0416a476e883f59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fa95d5d41a09c29fa43",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "196f0257e3b3f77f3f98",
+    "url": "/static/js/2.59af40a3.chunk.js"
+  },
+  {
+    "revision": "0445d5183d0141ac77c6557b72598011",
+    "url": "/static/js/2.59af40a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fa95d5d41a09c29fa43",
+    "url": "/static/js/main.e5f91e79.chunk.js"
+  },
+  {
+    "revision": "6d72c4d030b32a122061",
+    "url": "/static/js/runtime-main.a93d7415.js"
+  },
+  {
+    "revision": "a9063339b4863d51022004a1677b9c1e",
+    "url": "/static/media/cog.a9063339.svg"
+  },
+  {
+    "revision": "671822c6ac97a5833d10a799b134582f",
+    "url": "/static/media/emo.671822c6.svg"
+  },
+  {
+    "revision": "0864f32a428272ef18b20510c961a943",
+    "url": "/static/media/fin.0864f32a.svg"
+  },
+  {
+    "revision": "d0d785cc3529fb590c866e8d6a0e9418",
+    "url": "/static/media/logo.d0d785cc.png"
+  },
+  {
+    "revision": "95da910b6d5fe17e0408abf962abb10e",
+    "url": "/static/media/phy.95da910b.svg"
+  },
+  {
+    "revision": "10d304c63a971144aa143fbf749f0980",
+    "url": "/static/media/spi.10d304c6.svg"
+  }
+]);
